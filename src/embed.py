6a142705bd34d7b5b62787ae7b292fdda58ec58b
blazerius54 <79137837220@yandex.ru>
2026-05-17 23:16:33 +0300

    add collection logic

--- src/embed.py
+++ src/embed.py
@@ -1,5 +1,8 @@
 import json
 
+import chromadb 
+from chromadb.api.types import Metadata
+from sentence_transformers import SentenceTransformer
 from pathlib import Path
 
 from src.schemas import Chunk
@@ -9,6 +12,9 @@ ROOT_DIR = Path(__file__).resolve().parent.parent
 CHUNKS_FILE_PATH = ROOT_DIR / "output" / "chunks" / "chunks.jsonl"
 CHROMA_PATH = ROOT_DIR / "output" / "chroma"
 
+MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+
+COLLECTION_NAME = "documents"
 
 def load_chunks() -> list[Chunk]:
     chunks: list[Chunk] = []
@@ -24,7 +30,33 @@ def load_chunks() -> list[Chunk]:
 
 def main() -> None:
     CHROMA_PATH.mkdir(parents=True, exist_ok=True)
-    load_chunks()
+    chunks = load_chunks()
+
+    if not chunks:
+        print("No chunks found")
+        return
+    
+    model = SentenceTransformer(MODEL_NAME)
+
+    texts: list[str] = [chunk["text"] for chunk in chunks]
+    metadatas: list[Metadata] = [
+        {
+            "source": chunk["metadata"]["source"],
+            "chunk_index": chunk["metadata"]["chunk_index"],
+        }
+        for chunk in chunks
+    ]
+    ids: list[str] = [
+        f'{chunk["metadata"]["source"]}_{chunk["metadata"]["chunk_index"]}'
+        for chunk in chunks
+    ]
+
+    embeddings = model.encode(texts).tolist() 
+    client = chromadb.PersistentClient(path=str(CHROMA_PATH)) 
+    collection = client.get_or_create_collection(name=COLLECTION_NAME) 
+    collection.add( ids=ids, documents=texts, metadatas=metadatas, embeddings=embeddings, ) 
+    print(f"Saved {len(chunks)} chunks to Chroma")
+
 
 if __name__ == "__main__":
     main()
